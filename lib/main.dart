fe58705b6cfd716a7931ddceeb09d64e1f5985f8
wali4414764 <wali.specail07@gmail.com>
2023-04-06 23:55:39 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:waleed/Image_Page.dart';
 import 'package:waleed/pages/Home_page.dart';
+import 'package:waleed/pages/day3.dart';
+import 'package:waleed/pages/login%20page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget{
    return MaterialApp(
      debugShowCheckedModeBanner: false,
      title: "flutter 30 days challenge",
-     home: ImagePage(),
+     home: LoginPage(),
    );
   }
 
